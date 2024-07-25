83e25f60adf591b22ac86298e16307e665e46136
Zakria-04 <zakriaabdelgny04@gmail.com>
2024-07-25 23:27:47 +0300

    expo navigation

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Home from "@/src/screens/Home";
+
+const home = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Home />
+    </View>
+  );
+};
+
+export default home;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Home from "../src/screens/Home";
+import { Stack } from "expo-router";
 
 const _layout = () => {
   return (
-    <View style={{flex: 1,}}>
-      <Home />
-    </View>
+    <Stack screenOptions={{headerShown: false}}>
+      <Stack.Screen name="(tabs)" />
+    </Stack>
   );
 };
 
